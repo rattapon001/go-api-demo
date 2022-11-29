c93e1b3f25316554c761e5b55de0c9ce501e1eca
rattapon simsri <rattapon.simsri0@gmail.com>
2022-11-29 11:49:16 +0700

    delete comment

--- database/model/model.go
+++ database/model/model.go
@@ -7,10 +7,6 @@ import (
 
 func (m *MediaObject) BeforeCreate(tx *gorm.DB) (err error) {
 	m.Id = uuid.New()
-
-	// if u.Role == "admin" {
-	// 	return errors.New("invalid role")
-	// }
 	return
 }
 
@@ -26,7 +22,4 @@ type MediaObject struct {
 	Id            uuid.UUID `gorm:"primaryKey" gorm:"type:uuid;default:uuid_generate_v4()"`
 	FilePath      string    `json:"filePath"`
 	DirectoryName string    `json:"directoryName"`
-	// LastName  string `json:"lastName"`
-	// Age       int    `json:"age"`
-	// Email     string `json:"email"`
 }
